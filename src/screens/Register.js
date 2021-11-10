454444f07ad7bedd84210a2c232fc7bf561d310b
Shalini-CEG <ahana298@gmail.com>
2021-11-10 20:55:14 +0530

    Registration fixed!

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -2,11 +2,13 @@ import { View, StyleSheet, Text } from "react-native";
 import { TextInput, Card, Button, Title } from "react-native-paper";
 import { basic, dark } from "../default/colors";
 import Alert from "../components/Alert";
+import database from "../storage/firebase";
+import { getDatabase, ref, set } from "firebase/database";
 import React, { useState, useEffect } from "react";
 
-const Login = ({ navigation }) => {
+const Login = () => {
   const initalState = {
-    name: "",
+    mail: "",
     pwd: "",
     contact: "",
   };
@@ -18,58 +20,92 @@ const Login = ({ navigation }) => {
     <Alert alert={alert} setAlert={setAlert} />;
   }, [alert]);
 
-  const handleRegister = () => {};
+  const handleRegister = () => {
+    if (form.mail === "" || form.contact === "" || form.pwd === "")
+      setAlert("Fields are empty!");
+    else if (form.pwd.length < 5) setAlert("Password length mismatched!");
+    else if (form.contact.length < 10) setAlert("Contact length mismatched!");
+    else {
+      const db = getDatabase();
+
+      set(ref(db, "users/" + form.mail), {
+        pwd: form.pwd,
+        contact: form.contact,
+      });
+
+      setForm(initalState);
+      setAlert("User registered!");
+    }
+  };
 
   return (
     <>
       <View style={styles.container}>
-        <Card style={{ padding: 20 }}>
-          <Title
-            style={{ textAlign: "center", color: dark, marginVertical: 5 }}
-          >
-            Create Account
-          </Title>
+        {alert ? (
+          <Alert alert={alert} setAlert={setAlert} />
+        ) : (
+          <Card style={{ padding: 20 }}>
+            <Title
+              style={{
+                textAlign: "center",
+                color: basic,
+                marginVertical: 10,
+                textTransform: "uppercase",
+              }}
+            >
+              Create Account
+            </Title>
 
-          <TextInput
-            mode="outlined"
-            label="Email address"
-            placeholder="Enter email"
-            // style={{ marginBottom: 10 }}
-            outlineColor={basic}
-            activeOutlineColor={dark}
-          />
+            <TextInput
+              mode="outlined"
+              value={form.mail}
+              label="Email address"
+              placeholder="Enter email"
+              // style={{ marginBottom: 10 }}
+              outlineColor={basic}
+              activeOutlineColor={dark}
+              onChangeText={(text) => setForm({ ...form, mail: text })}
+            />
 
-          <TextInput
-            mode="outlined"
-            label="Contact"
-            placeholder="Enter contact number"
-            // style={{ marginBottom: 10 }}
-            outlineColor={basic}
-            activeOutlineColor={dark}
-            keyboardType="numeric"
-            right={<TextInput.Affix text="/10" />}
-          />
+            <TextInput
+              mode="outlined"
+              value={form.contact}
+              label="Contact"
+              placeholder="Enter contact number"
+              // style={{ marginBottom: 10 }}
+              outlineColor={basic}
+              activeOutlineColor={dark}
+              keyboardType="numeric"
+              maxLength={10}
+              right={<TextInput.Affix text="/10" />}
+              onChangeText={(num) => setForm({ ...form, contact: num })}
+            />
 
-          <TextInput
-            mode="outlined"
-            k
-            label="Password"
-            placeholder="Enter secret code"
-            outlineColor={basic}
-            activeOutlineColor={dark}
-          />
+            <TextInput
+              mode="outlined"
+              value={form.pwd}
+              label="Password"
+              secureTextEntry
+              placeholder="Enter secret code"
+              outlineColor={basic}
+              activeOutlineColor={dark}
+              onChangeText={(text) => setForm({ ...form, pwd: text })}
+            />
+            <Text style={{ fontWeight: "bold", color: "red" }}>
+              Enter aleast 5 characters
+            </Text>
 
-          <Button
-            icon="import"
-            mode="contained"
-            style={{ marginTop: 20, padding: "2%" }}
-            color={basic}
-            onPress={handleRegister}
-          >
-            Register !
-          </Button>
+            <Button
+              icon="import"
+              mode="contained"
+              style={{ marginTop: 20, padding: "2%" }}
+              color={basic}
+              onPress={handleRegister}
+            >
+              Register !
+            </Button>
 
-          <Text style={{ marginVertical: 10 }}>
+            {/* <Text style={{ marginVertical: 10 }}>
             Already a user,{" "}
             <Text
               style={{ fontWeight: "bold", color: "red" }}
@@ -77,8 +113,9 @@ const Login = ({ navigation }) => {
             >
               Sign in
             </Text>
-          </Text>
-        </Card>
+          </Text> */}
+          </Card>
+        )}
       </View>
     </>
   );
