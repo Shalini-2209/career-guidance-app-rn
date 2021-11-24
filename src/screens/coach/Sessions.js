10e36af8cb2a11945ec76c425531dbf3922f7f24
Shalini-CEG <ahana298@gmail.com>
2021-11-24 23:53:00 +0530

    Sharing Meeting link

--- src/screens/coach/Sessions.js
+++ src/screens/coach/Sessions.js
@@ -1,50 +1,130 @@
-import { ScrollView } from "react-native";
-import { List } from "react-native-paper";
-import { onValue } from "firebase/database";
+import { ScrollView, Text, StyleSheet } from "react-native";
+import { List, Surface } from "react-native-paper";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  update,
+  remove,
+  set,
+} from "firebase/database";
 import { dark } from "../../default/colors";
 import React, { useState, useEffect } from "react";
 import { getRef } from "../../services/api-services";
+import { sendEmail } from "../../components/sendEmail";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Sessions = () => {
   // booked sessions
   const [bookings, setBookings] = useState([]);
+  const [msg, setMsg] = useState("");
+  const db = getDatabase();
 
   useEffect(() => {
     const fetchData = async () => {
-      let curRef = await getRef("bookings/", "coach");
+      const curRef = await getRef("bookings/", "coach");
       onValue(curRef, (snapshot) => {
-        setBookings(snapshot.val());
+        if (snapshot.exists()) setBookings(snapshot.val());
+        else setMsg("No upcoming sessions!");
       });
     };
     fetchData();
   }, []);
 
-  console.log(bookings);
+  // console.log(bookings);
 
+  const deleteBooking = (uid, cid, bookingId) => {
+    const userRef = ref(db, "/bookings/" + uid + "/" + bookingId);
+    const coachRef = ref(db, "/bookings/" + cid + "/" + bookingId);
+
+    remove(userRef);
+    // .then(() => {
+    //   console.log("Deleted");
+    // })
+    // .catch((error) => {
+    //   console.log({ error });
+    // });
+    remove(coachRef);
+  };
+
+  const handleCompletedSession = async (uname, bookingId) => {
+    const cid = await AsyncStorage.getItem("coach");
+    const url = "coaches/" + cid + "/slots";
+
+    const newRef = ref(db, url);
+
+    onValue(
+      newRef,
+      (snapshot) => {
+        let available = snapshot.val() + 1;
+        console.log(available);
+        update(ref(db, "coaches/" + cid + "/"), { slots: available })
+          .then(() => {
+            console.log("Updated");
+          })
+          .catch((error) => {
+            console.log({ error });
+          });
+      },
+      {
+        onlyOnce: true,
+      }
+    );
+
+    deleteBooking(uname, cid, bookingId);
+  };
+
+  const handleMeeting = (name, bookingId) => {
+    onValue(ref(db, "/users/" + name), (snapshot) => {
+      let mail = snapshot.val().email;
+      sendEmail(
+        mail,
+        "Career guidance Meeting Invitation ",
+        " We invite you to a meeting which will last approximately an hour."
+      ).then(() => {
+        handleCompletedSession(name, bookingId);
+      });
+    });
+  };
   return (
     <ScrollView style={{ flexGrow: 1, backgroundColor: "white" }}>
-      {Object.keys(bookings).map((elt) => {
-        return (
-          <List.Item
-            key={bookings[elt].name}
-            title={bookings[elt].name + " is waiting for your session"}
-            description={"Session time: " + bookings[elt].date}
-            left={(props) => (
-              <List.Icon
-                {...props}
-                icon="account-circle"
-                style={{
-                  backgroundColor: dark,
-                  borderRadius: "50%",
-                }}
-                color="white"
+      {bookings.length ? (
+        Object.keys(bookings).map((elt) => {
+          return (
+            <Surface style={styles.surface} key={bookings[elt].name}>
+              <List.Item
+                title={bookings[elt].name + " is waiting for your session"}
+                description="Click here to send meeting link..."
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
+                left={(props) => (
+                  <List.Icon
+                    {...props}
+                    icon="account-circle"
+                    style={{
+                      backgroundColor: dark,
+                      borderRadius: "50%",
+                    }}
+                    color="white"
+                  />
+                )}
+                onPress={() => handleMeeting(bookings[elt].name, elt)}
               />
-            )}
-          />
-        );
-      })}
+            </Surface>
+          );
+        })
+      ) : (
+        <Text> No upcoming sessions </Text>
+      )}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  surface: {
+    marginBottom: 2,
+    elevation: 5,
+  },
+});
 
 export default Sessions;
