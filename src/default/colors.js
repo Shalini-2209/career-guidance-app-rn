c1a7e0f3b293126dbd28f054af97cdeebf36ea66
Shalini-CEG <ahana298@gmail.com>
2021-11-29 19:20:13 +0530

    Altered app theme

--- src/default/colors.js
+++ src/default/colors.js
@@ -1,5 +1,7 @@
-const basic = "#2980b9";
-const bgcolor = "#2c3e50";
-const dark = "#0d84ff";
+// const basic = "#2980b9";
+const basic = "#50396c";
+const bgcolor = "#181120";
+// const dark = "#0d84ff";
+const dark = "#43305a";
 
 export { basic, dark, bgcolor };
