d6e3ac61516400ddc3ff97f8b8b78a2ad7c1f5d8
Shalini-CEG <ahana298@gmail.com>
2021-11-29 19:20:33 +0530

    Isolated Loading

---
+++ src/components/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ActivityIndicator } from "react-native-paper";
+
+const Loading = () => {
+  return <ActivityIndicator animating={true} color="#1e3799" />;
+};
+
+export default Loading;
